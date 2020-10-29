f4b10a96a93a852773ca9d60c0265f39c29a2ce0
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2020-10-29 21:05:12 +0630

    Initial Setup with Firebase Setup

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:brew_crew/screens/authenticate/authenticate.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return Home or Authenticate Widget
+    return Authenticate();
+  }
+}
---
+++ lib/services/auth.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+
+  // sign in anony
+
+  //with email, password
+
+  //register with email, password
+
+  //sign out
+  
+}
